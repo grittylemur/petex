2cc59430fa1c95d5bc335884cf1b9876d646e76b
Lemur Gritty <lemurgritty@gmail.com>
2019-04-01 14:38:09 -0400

    User dashbxoard: My Pets WIP

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,9 +1,21 @@
 const express = require("express")
+const Pet = require("../models/pet")
 let router = express.Router()
 
 const dashboardRouter = (app) => {
     router.get("/", function(req, res){
-        res.render("dashboard/index")
+        Pet.find({owner: app.locals.currentUser.id} ,function(err, pets) {
+            res.render("dashboard/index", {pets})
+        })
+        
+    })
+
+    router.get("/:id/delete", function(req, res){
+        const id = req.params.id
+        Pet.remove({_id: id}, function(err){
+            if(err) console.log(err)
+            res.redirect("/dashboard")
+        })
     })
     return router
 }
